7e52907f6c8c91816bd19f1856fa8a9505c85610
GuuTAR <guitar20122012@gmail.com>
2020-06-25 01:52:40 +0700

    upload vote

--- routes/vote.js
+++ routes/vote.js
@@ -10,26 +10,28 @@ const submitVote = require('../methods/vote').submitVote;
 /* GET home page. */
 
 router.get('/:topic_id/vote', async (req, res) => {
-    
-    const result = await getVote()
-    
-    results = []
-    result.forEach(item => {
-        if (parseInt(item.dataValues.topic_id) === parseInt(req.params.topic_id)) {
-            results.push(item.dataValues)
-        }
-    })
-    res.json(results)
-    res.status(404);
-    res.end();
+    const channel_key = req.headers['channel-key']
+
+    const status = await ideate.checkPermission(channel_key, req.params.topic_id);
+
+    if (status) {
+        const result = await await getVote(req.params.topic_id)
+        res.json(result);
+        res.status(200);
+        res.end();
+
+    } else {
+        res.status(404);
+        res.end();
+    }
 })
 
 router.post('/:topic_id/vote', async (req, res) => {
     const channel_key = req.headers['channel-key'];
+ 
     const status = await ideate.checkPermission(channel_key, req.params.topic_id);
-
     if (status) {
-        const result = await submitVote(req.params.topic_id, req.body.group_id, req.body.username)
+        const result = await submitVote(req.params.topic_id, req.body.group_id, req.body.user_name)
         if (result === 'Already') {
             res.json('Already');
             res.end();
